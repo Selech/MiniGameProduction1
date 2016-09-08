3b6ea680b8aeaeb8e709f587c6cacb3c4463bcf2
Nicolai <nico_goal@hotmail.com>
2016-09-08 13:51:24 +0200

    Added Player Object

--- Assets/_TheProject/_Scenes/Prototypes/Nicolai Prototype/PlayerController.cs
+++ Assets/_TheProject/_Scenes/Prototypes/Nicolai Prototype/PlayerController.cs
@@ -3,21 +3,34 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float tiltSpeed = 0.0f; 
+	[Range(40.0f, 60.0f)]
+	public float rotationMultiplier = 50f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+	[Range(5.0f, 15.0f)]
+	public float rotationSpeed = 10f;
+
+	[Range(10.0f, 30.0f)]
+	public float strafeReduction = 20f;
+
+	private float rotationAngle = 0f;
+
+	[Range(0.01f, 0.1f)]
+	public float forwardSpeed = 0.05f;
 
 	void FixedUpdate(){
-		Vector3 movement = new Vector3 (Input.acceleration.x, 0.0f, 0.0f);
-		this.GetComponent<Rigidbody>().velocity = movement * tiltSpeed;
-	}
+		
+		var target = Quaternion.Euler (0, 0, -Input.acceleration.x * rotationMultiplier);
+		transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime*rotationSpeed);
 
+		if (transform.rotation.eulerAngles.z > 180) {
+			rotationAngle = 360 - transform.rotation.eulerAngles.z;
+		} else {
+			rotationAngle = - transform.rotation.eulerAngles.z;
+		}
 
-	// Update is called once per frame
-	void Update () {
-	
+		transform.position = Vector3.MoveTowards(transform.position, new Vector3(rotationAngle, transform.position.y, transform.position.z), 1/strafeReduction);
+
+		transform.Translate(new Vector3(0f, 0f, forwardSpeed));
+
 	}
 }
