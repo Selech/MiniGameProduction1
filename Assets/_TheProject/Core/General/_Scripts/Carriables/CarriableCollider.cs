3704ec6718e74bb60fecf1c7340b382862b4789a
Rasmus Haarslev <rasmus.haarslev@gmail.com>
2016-09-08 11:48:00 +0200

    Adds a collider that destroys the object if it hits the ground

---
+++ Assets/_TheProject/Core/General/_Scripts/Carriables/CarriableCollider.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarriableCollider : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision) {
+		if (GameObject.FindWithTag ("Ground")) {
+			Destroy (gameObject);
+		}
+	}
+}
+	
