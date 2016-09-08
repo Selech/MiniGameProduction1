a85d702199f529c2ceea5f906e65796e55b7aff7
Rasmus Haarslev <rasmus.haarslev@gmail.com>
2016-09-08 12:02:21 +0200

    Changes Destroy object to an event

--- Assets/_TheProject/Core/Carriable/Scripts/CarriableCollider.cs
+++ Assets/_TheProject/Core/Carriable/Scripts/CarriableCollider.cs
@@ -3,9 +3,15 @@ using System.Collections;
 
 public class CarriableCollider : MonoBehaviour {
 
+
+	/// <summary>
+	/// Raises the collision enter event for the Carriable prefab. 
+	/// Ignores all collisions except for collisions with the ground, and triggers the LoseCarriableEvent.
+	/// </summary>
+	/// <param name="collision">Collision.</param>
 	void OnCollisionEnter(Collision collision) {
-		if (GameObject.FindWithTag ("Ground")) {
-			Destroy (gameObject);
+		if (collision.gameObject.CompareTag("Ground")) {
+			EventManager.TriggerEvent ("LoseCarriableEvent");
 		}
 	}
 }
