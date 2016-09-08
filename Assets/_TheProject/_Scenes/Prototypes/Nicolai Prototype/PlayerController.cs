c5a7f1e628dca0133ff09767b1227a295327dffe
Nicolai <nico_goal@hotmail.com>
2016-09-08 11:41:31 +0200

    Added PlayerController

--- Assets/_TheProject/_Scenes/Prototypes/Nicolai Prototype/PlayerController.cs
+++ Assets/_TheProject/_Scenes/Prototypes/Nicolai Prototype/PlayerController.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public float tiltSpeed = 0.0f; 
+
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
+	void FixedUpdate(){
+		Vector3 movement = new Vector3 (Input.acceleration.x, 0.0f, 0.0f);
+		this.GetComponent<Rigidbody>().velocity = movement * tiltSpeed;
+	}
+
+
 	// Update is called once per frame
 	void Update () {
 	
