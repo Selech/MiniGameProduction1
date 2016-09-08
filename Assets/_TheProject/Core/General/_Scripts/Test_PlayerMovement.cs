0b1294e90fe8063101a3b295af7f49ed17ad199d
Veso <veselin.yovchev@hotmail.com>
2016-09-08 15:09:32 +0200

    camera script

--- Assets/_TheProject/Core/General/_Scripts/Test_PlayerMovement.cs
+++ Assets/_TheProject/Core/General/_Scripts/Test_PlayerMovement.cs
@@ -5,7 +5,7 @@ public class Test_PlayerMovement : MonoBehaviour {
 	
 	public float speed = 10;
 	public float tilt = 3;
-
+	float moveHorizontal;
 	Rigidbody rigid;
 	// Use this for initialization
 	void Awake () {
@@ -15,13 +15,22 @@ public class Test_PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+		Move ();
+
+	}
+
+	void Move()
+	{
+		if (Application.isEditor) {
+			moveHorizontal = Input.GetAxis ("Horizontal");
+
+		} else {
+			moveHorizontal = Input.acceleration.x;
+		}
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 1);
 		rigid.velocity = movement * speed;
 
 		rigid.rotation = Quaternion.Euler (0.0f, 0.0f, rigid.velocity.x * -tilt);
-
 	}
 }
