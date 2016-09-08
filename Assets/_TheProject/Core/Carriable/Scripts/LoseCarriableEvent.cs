07a188e06ac71b7a7e2dcfcae5a7755edbcee8bb
Rasmus Haarslev <rasmus.haarslev@gmail.com>
2016-09-08 13:04:12 +0200

    Adds LoseCarriableEvent script

---
+++ Assets/_TheProject/Core/Carriable/Scripts/LoseCarriableEvent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCarriableEvent : MonoBehaviour {
+
+	void OnEnable()
+	{
+		EventManager.StartListening ("LoseCarriableEvent", LoseCarriable);
+	}
+
+	void OnDisable()
+	{
+		EventManager.StopListening ("LoseCarriableEvent", LoseCarriable);
+	}
+
+	void LoseCarriable() {
+
+	}
+}
