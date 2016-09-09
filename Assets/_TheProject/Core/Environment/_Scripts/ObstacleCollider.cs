cccef8fb8230cfc7c2dc084669bc46c924dccc26
Rasmus Haarslev <rasmus.haarslev@gmail.com>
2016-09-09 13:02:05 +0200

    Adds prefabs for all the obstacles, including the obstacle colliders

--- Assets/_TheProject/Core/Environment/_Scripts/ObstacleCollider.cs
+++ Assets/_TheProject/Core/Environment/_Scripts/ObstacleCollider.cs
@@ -3,8 +3,12 @@ using System.Collections;
 
 public class ObstacleCollider : MonoBehaviour {
 
+	public bool destroyOnCollision = true;
+	public float force = 0;
+
 	void OnCollisionEnter(Collision collision) {
+		GameManager.Instance.obstacleForceAddUp = force;
 		EventManager.TriggerEvent ("ObstacleHitEvent");
-		this.gameObject.SetActive (false);
+		this.gameObject.SetActive (destroyOnCollision);
 	}
 }
