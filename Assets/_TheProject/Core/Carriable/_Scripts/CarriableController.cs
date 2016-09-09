6138d77c6c09a09743d4755bb0de8823d570eac5
Andreea Muscalagiu <m_andreea93@yahoo.com>
2016-09-09 18:03:05 +0200

    Add full player with objects

--- Assets/_TheProject/Core/Carriable/_Scripts/CarriableController.cs
+++ Assets/_TheProject/Core/Carriable/_Scripts/CarriableController.cs
@@ -5,12 +5,14 @@ public class CarriableController : MonoBehaviour {
 	FixedJoint joint;
 	GameObject connectedBody;
 
+	public float breakForce = 40;
+
 	void Start(){
 		joint = GetComponent<FixedJoint> ();
 		connectedBody = joint.connectedBody.gameObject;
 	}
 
 	void OnJointBreak(){
-		connectedBody.GetComponent<FixedJoint> ().breakForce = 80;
+		connectedBody.GetComponent<FixedJoint> ().breakForce = breakForce;
 	}
 }
