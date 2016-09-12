8f3d34e5ec5844b3dfa2c64153603ae8bb8b9252
Nils Rungholm Jensen <nilsrungholmjensen@gmail.com>
2016-09-12 17:31:51 +0200

    Tweaked building spawner + level adjustments

--- Assets/_TheProject/Core/Tools/SpawnBuildings.cs
+++ Assets/_TheProject/Core/Tools/SpawnBuildings.cs
@@ -19,6 +19,7 @@ public class SpawnBuildings : EditorWindow {
 
 
 	float positionJitter = 0f;
+	float zPositionJitter = 0f;
 	float rotateJitter = 0f;
 	float houseWidth = 3f;
 	int length = 20;
@@ -40,7 +41,7 @@ public class SpawnBuildings : EditorWindow {
 				GameObject go;
 				GameObject insGo = houses[Random.Range(0,9)]; 
 
-				go = (GameObject)Instantiate(insGo, parent.transform.position + new Vector3( parent.transform.position.x + Random.Range(0,positionJitter), parent.transform.position.y,parent.transform.position.z*2 + i * width ),parent.transform.rotation,parent.transform);
+				go = (GameObject)Instantiate(insGo, parent.transform.position + new Vector3( parent.transform.position.x + Random.Range(0,positionJitter), /*parent.transform.position.y*/insGo.transform.position.y,(parent.transform.position.z*2 + i * width) + Random.Range(0,zPositionJitter) ),parent.transform.rotation,parent.transform);
 				go.transform.Rotate(0, Random.Range (-rotateJitter, rotateJitter), 0);
 			}
 		}
@@ -50,7 +51,7 @@ public class SpawnBuildings : EditorWindow {
 		EditorGUILayout.Space ();
 		parent = (GameObject)EditorGUI.ObjectField(new Rect (3, 3, position.width - 12, 20),"Find Parent", parent, typeof(GameObject));
 		EditorGUILayout.Space ();
-		house0 = ((GameObject)EditorGUI.ObjectField (new Rect (3, 150, position.width - 12, 20), "Find GameObject", house0, typeof(GameObject)));
+
 		house1 = (GameObject)EditorGUI.ObjectField (new Rect (3, 170, position.width - 12, 20), "Find GameObject", house1, typeof(GameObject));
 		house2 = (GameObject)EditorGUI.ObjectField (new Rect (3, 190, position.width - 12, 20), "Find GameObject", house2, typeof(GameObject));
 		house3 = (GameObject)EditorGUI.ObjectField (new Rect (3, 210, position.width - 12, 20), "Find GameObject", house3, typeof(GameObject));
@@ -60,6 +61,7 @@ public class SpawnBuildings : EditorWindow {
 		house7 = (GameObject)EditorGUI.ObjectField (new Rect (3, 290, position.width - 12, 20), "Find GameObject", house7, typeof(GameObject));
 		house8 = (GameObject)EditorGUI.ObjectField (new Rect (3, 310, position.width - 12, 20), "Find GameObject", house8, typeof(GameObject));
 		house9 = (GameObject)EditorGUI.ObjectField (new Rect (3, 330, position.width - 12, 20), "Find GameObject", house9, typeof(GameObject));
+		house0 = ((GameObject)EditorGUI.ObjectField (new Rect (3, 350, position.width - 12, 20), "Find GameObject", house0, typeof(GameObject)));
 		EditorGUILayout.Space ();
 
 			if (GUILayout.Button ("Generate Buildings")) {
@@ -69,9 +71,11 @@ public class SpawnBuildings : EditorWindow {
 		EditorGUILayout.Space ();
 		positionJitter = EditorGUILayout.Slider ("Position Jitter",positionJitter,-10f, 10f);
 		EditorGUILayout.Space ();
+		zPositionJitter = EditorGUILayout.Slider ("Z Position Jitter",zPositionJitter,-10f, 10f);
+		EditorGUILayout.Space ();
 		rotateJitter = EditorGUILayout.Slider ("Rotaton Jitter",rotateJitter,-20f, 20f);
 		EditorGUILayout.Space ();
-		length = EditorGUILayout.IntSlider("Length",length,0, 200);
+		length = EditorGUILayout.IntSlider("Length",length,0, 500);
 		EditorGUILayout.Space ();
 		width = EditorGUILayout.Slider("Width of House",width,0, 100f);
 
