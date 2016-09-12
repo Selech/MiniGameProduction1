6108eb828f8d6a04446c14ab9e21f2cbdb068b46
Peter Scheel <selech07@gmail.com>
2016-09-12 12:12:05 +0200

    Updated player control + some Wwise stuff

--- Assets/_TheProject/Core/Player/PlayerController.cs
+++ Assets/_TheProject/Core/Player/PlayerController.cs
@@ -26,9 +26,19 @@ public class PlayerController : MonoBehaviour {
 	[Range(0.0f,1.0f)]
 	public float yThreshold = 0.4f;
 
+	[Range(1.0f,10.0f)]
+	public float boostAmount = 8f;
+
+	[Range(1.0f,10.0f)]
+	public float brakeAmount = 5f;
+
+	[Range(1,10)]
+	public int powerUpTime = 5;
+
 	public Rigidbody body;
 	private float oldY;
-	private bool flipped = false;
+	private bool boost = false;
+	private bool brake = false;
 
 	void Start()
 	{
@@ -52,16 +62,15 @@ public class PlayerController : MonoBehaviour {
 		Move ();
 	}
 
-	void Move()
-	{
-		oldY = oldY == 0 ?  Input.acceleration.y : oldY ;
-		//Debug.Log (Input.acceleration.x);
+	void Move() {
+
+		/* SIDE MOVEMENT */
+
 		float sideSpeed = Mathf.Abs (Input.acceleration.x) > deadZone ? Input.acceleration.x : 0;
 		var target = Quaternion.Euler (0, 0, -sideSpeed * rotationMultiplier);
 
 		body.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime*rotationSpeed);
-		//transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime*rotationSpeed);
-
+	
 		if (transform.rotation.eulerAngles.z > 180) {
 			rotationAngle = 360 - transform.rotation.eulerAngles.z;
 		} else {
@@ -70,32 +79,45 @@ public class PlayerController : MonoBehaviour {
 
 		rotationAngle /= 500;
 
-		transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x + rotationAngle, transform.position.y, transform.position.z), 1/strafeReduction);
+		var x = Mathf.Abs (rotationAngle) > deadZone/500 && Mathf.Abs(body.velocity.x) > 0.00025 ? body.velocity.x + rotationAngle : 0;
 
-		if (oldY - Input.acceleration.y < -yThreshold && !flipped) {
+		body.velocity = new Vector3 (x, body.velocity.y, body.velocity.z);
+		//transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x + rotationAngle, transform.position.y, transform.position.z), 1/strafeReduction);
+
+		/* SPEED-UP / BRAKE MOVEMENT */
+
+		oldY = oldY == 0 ?  Input.acceleration.y : oldY ;
+
+		if (oldY - Input.acceleration.y < -yThreshold && !boost && !brake) {
 			print ("Boost");
-			flipped = true;
+			boost = true;
+			StartCoroutine (Flip(powerUpTime));
 		}
-		else if (oldY - Input.acceleration.y > yThreshold && !flipped) {
+		else if (oldY - Input.acceleration.y > yThreshold && !boost && !brake) {
 			print ("Brake");
-			flipped = true;
+			brake = true;
+			StartCoroutine (Flip(powerUpTime));
 		}
 
-		if (Input.acceleration.y > 0.1f) {
-			maxSpeed += Input.acceleration.y;
-		} else if( Input.acceleration.y < -0.1f) {
-			maxSpeed += Input.acceleration.y;
+		if(body.velocity.z < (boost ? maxSpeed + boostAmount : maxSpeed)){
+			body.AddForce (new Vector3(0f, 0f, (boost ? forwardSpeed + 2 : forwardSpeed)), ForceMode.VelocityChange);	
 		}
-
-		if(body.velocity.z < maxSpeed){
-			body.AddForce (new Vector3(0f, 0f, forwardSpeed), ForceMode.VelocityChange);	
+		else if(body.velocity.z < (brake ? maxSpeed - brakeAmount : maxSpeed)){
+			body.AddForce (new Vector3(0f, 0f, forwardSpeed), ForceMode.VelocityChange);
 		}
 
 		oldY = Input.acceleration.y;
+
 	}
 
 	void Jump(){
 		AkSoundEngine.PostEvent ("Play_Collision", this.gameObject);
 		body.AddForce (new Vector3(0,GameManager.Instance.obstacleForceAddUp,0), ForceMode.VelocityChange);
+	}
+
+	IEnumerator Flip(int waitSec){
+		yield return new WaitForSeconds (waitSec);
+		boost = false;
+		brake = false;
 	}
 }
