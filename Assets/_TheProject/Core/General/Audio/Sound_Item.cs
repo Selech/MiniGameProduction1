d6c249d251dfcbc9034ad347a30be3b26591af9b
Veso <veselin.yovchev@hotmail.com>
2016-09-12 10:24:46 +0200

    begin making sound db, add sound event container to game controller

---
+++ Assets/_TheProject/Core/General/Audio/Sound_Item.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "SoundItem",menuName = "SoundItem")]
+public class Sound_Item : ScriptableObject 
+{
+	public int soundIndex;
+	public string soundEventName;
+	public float soundDuration;
+}
