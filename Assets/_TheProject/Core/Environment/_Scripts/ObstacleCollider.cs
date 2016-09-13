9fc1c53b624774449d626f77dd25992cb3aabc20
Nicolai <nico_goal@hotmail.com>
2016-09-13 11:15:52 +0200

    Added animation effect on obstacle detection

--- Assets/_TheProject/Core/Environment/_Scripts/ObstacleCollider.cs
+++ Assets/_TheProject/Core/Environment/_Scripts/ObstacleCollider.cs
@@ -11,10 +11,13 @@ public class ObstacleCollider : MonoBehaviour {
 	[Range(0.0f, 100.0f)]
 	public float brakeForce = 0;
 
+	public GameObject particles;
+
 	void OnTriggerEnter(Collider other) {
 		GameManager.Instance.obstacleForceAddUp = jumpForce;
 		GameManager.Instance.obstacleBrakeForce = brakeForce;
 		EventManager.TriggerEvent (GameManager.Instance._eventsContainer.obstacleHit);
+		Instantiate (particles, this.transform.position, Quaternion.identity);
 		this.gameObject.SetActive (!destroyOnCollision);  
 	}
 }
