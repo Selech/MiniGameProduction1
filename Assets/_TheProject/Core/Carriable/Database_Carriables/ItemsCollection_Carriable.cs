eb001ffbb11cf57c0c526c6316236f2042318938
John <irelandjj@gmail.com>
2016-09-13 16:37:20 +0200

    UVs for the bike and carriables, remade the stacking scene

--- Assets/_TheProject/Core/Carriable/Database_Carriables/ItemsCollection_Carriable.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-[CreateAssetMenu(fileName = "ItemsCollection_Carriable",menuName = "ItemsCollection_Carriable")]
-public class ItemsCollection_Carriable : ScriptableObject {
-	[SerializeField]
-	public List<Item_Carriable> itemsCollection = new List<Item_Carriable>();
-}
